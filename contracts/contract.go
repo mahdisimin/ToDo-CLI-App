a00ad274923b5059c03d861d1b79da1550a71469
= <mohammadmahdisimin@gmail.com>
2026-02-08 14:53:03 +0330

    minor modification

--- contracts/contract.go
+++ contracts/contract.go
@@ -12,3 +12,9 @@ const (
 	TaskFilePath     = ".//TaskList.text"
 	CategoryFilePath = ".//CategoryList.text"
 )
+
+// Network configuration
+const (
+	network = "tcp"
+	address = "127.0.0.1:8080"
+)
